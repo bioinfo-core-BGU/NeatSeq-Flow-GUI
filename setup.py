f8fafd90c873b0218b4b92a8f1b460ee52c778dc
levinbgu <levinl@post.bgu.ac.il>
2019-06-13 17:36:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages, setup,os
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
-        for (dir ,filename) in (directories,filenames):
-            paths.append((dir , os.path.join( path, filename)))
+        for filename in filenames:
+            paths.append((  os.path.join(directory,path.split(directory)[1]) , os.path.join( path, filename)))
     return paths
 
 extra_files = package_files("neatseq_flow_gui")
